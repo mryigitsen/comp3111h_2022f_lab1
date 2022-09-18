3d3e5f799e13ebe055d69e766194213a93ad6c86
Yigit Sen <ysen@connect.ust.hk>
2022-09-18 19:43:52 +0800

    Updated Library.java

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -7,7 +7,11 @@ public class Library {
 	
 	public static void main(String args[])
 	{
-		System.out.println("Hello, world!");
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
+		{
+			System.out.println("Hello, world!");
+		}
 	}
 	
     public boolean someLibraryMethod() {
